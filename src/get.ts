5f7142a726c029cd822cb5bde4713d8e154ba9d0
Chand <artchand@a483e70125d5.ant.amazon.com>
2020-02-06 15:08:55 -0600

    added code files

--- src/get.ts
+++ src/get.ts
@@ -1,5 +1,5 @@
 import { DynamoDB } from 'aws-sdk'
-
+const magics = require('./magics');
 const ddb = new DynamoDB.DocumentClient()
 
 /**
@@ -14,37 +14,64 @@ const ddb = new DynamoDB.DocumentClient()
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
  * 
  */
+
 export const handler = async (event: any = {}): Promise<any> => {
-    let response
-    try {
-      const params = {
-        TableName: 'data-table',
-        Key: {
-          data: event.pathParameters.data
-        }
-      }
-
-      const results = await ddb.get(params).promise()
-
-      if (!results || !results.Item) {
-        response = {
-          headers: {'Access-Control-Allow-Origin': '*'},
-          statusCode: 400,
-        }
+  let response;
+  try {
+      const results = magics.canHave(event.pathParameters.data);
+      if (!results) {
+          response = {
+              headers: { 'Access-Control-Allow-Origin': '*' },
+              statusCode: 400,
+          }
       } else {
-        response = {
-          headers: {'Access-Control-Allow-Origin': '*'},
-          statusCode: 200,
-          body: JSON.stringify(results.Item),
-        }
+          response = {
+              headers: { 'Access-Control-Allow-Origin': '*' },
+              statusCode: 200,
+              body: JSON.stringify(results),
+          }
       }
-    } catch (err) {
+  } catch (err) {
       console.log(err)
       response = {
-        headers: {'Access-Control-Allow-Origin': '*'},
-        statusCode: 500,
+          headers: { 'Access-Control-Allow-Origin': '*' },
+          statusCode: 500,
       }
-    }
-
-    return response
+  }
+  return response
 }
+
+// export const handler = async (event: any = {}): Promise<any> => {
+//     let response
+//     try {
+//       const params = {
+//         TableName: 'data-table',
+//         Key: {
+//           data: event.pathParameters.data
+//         }
+//       }
+
+//       const results = await ddb.get(params).promise()
+
+//       if (!results || !results.Item) {
+//         response = {
+//           headers: {'Access-Control-Allow-Origin': '*'},
+//           statusCode: 400,
+//         }
+//       } else {
+//         response = {
+//           headers: {'Access-Control-Allow-Origin': '*'},
+//           statusCode: 200,
+//           body: JSON.stringify(results.Item),
+//         }
+//       }
+//     } catch (err) {
+//       console.log(err)
+//       response = {
+//         headers: {'Access-Control-Allow-Origin': '*'},
+//         statusCode: 500,
+//       }
+//     }
+
+//     return response
+// }
---
+++ src/magicRepository.js
@@ -0,0 +1,68 @@
+//'use strict';
+const dataProvider = require('./datamanager');
+/****** (PARAMS) - Table Name  */
+var playerTablename = 'player';
+var magicTableName = 'magic';
+
+/**************************** */
+
+/********* Query strcutures for all the tables */
+// var playerQuery = {
+//   TableName: playerTablename,
+//   ProjectionExpression: '#playerid, experience, #items',
+//   ExpressionAttributeNames: {
+//     "#playerid": "playerid",
+//     "#items": "items"
+//   },
+//   FilterExpression: "#playerid = :pId",
+//   ExpressionAttributeValues: {
+//     ":pId": {
+//       "N": "1"
+//     }
+//   }
+//   //TableName: program.table,
+//   //Limit: 1000
+// };
+
+var magicQuery = {
+  TableName: magicTableName,
+  ProjectionExpression: 'magicid, magic_name, experience_required, magic_items',
+};
+
+async function getPlayerInfo(playerid) {
+  // playerQuery.Key = {
+  //   "playerid": playerid
+  // };
+
+  const playerQuery = {
+    TableName: playerTablename,
+    Key: {
+      "playerid": playerid
+    },
+    ProjectionExpression: '#playerid, experience, #itemList',
+    ExpressionAttributeNames: {
+      "#playerid": "playerid",
+      "#itemList": "items"
+    },
+    // FilterExpression: "#playerid = :pId",
+    // ExpressionAttributeValues: {
+    //   ":pId": {
+    //     "N": "1"
+    //   }
+    // }
+    //TableName: program.table,
+    //Limit: 1000
+  };
+
+
+  return await dataProvider.queryTable(playerQuery, 'player');
+}
+
+async function getMagicList() {
+  return await dataProvider.scanTable(magicQuery);
+}
+
+module.exports = {
+  getPlayerInfo,
+  getMagicList
+};
